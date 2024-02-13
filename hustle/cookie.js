9d1fc301227b74b70bae2aff399ed47cfd4f7672
dafyn <aromorachd@gmail.com>
2024-02-13 16:10:29 +0300

    Initial Commit

---
+++ hustle/cookie.js
@@ -0,0 +1,24 @@
+let slideIndex = 0;
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        dots[i].classList.remove("active");
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].classList.add("active");
+
+    setTimeout(showSlides, 5000); // Change slide every 5 seconds
+}
+
+showSlides();
